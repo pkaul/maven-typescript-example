2e4f0dce39a8c722b8010fa8c645c3961da4673c
Peter Kaul <pkaul@bigpoint.net>
2014-03-07 13:52:00 +0100

    Fixing reference

--- example-webapp/src/main/webapp/modules/application.js
+++ example-webapp/src/main/webapp/modules/application.js
@@ -2,11 +2,11 @@
  * Bootstraps the example application:
  *  Loads the 'demo' module from 'example-library-js' via RequireJS and instantiates some logic from this library
  */
-require(['example-library-js/demo'], function(demo) {
+require(['example-library-js/demo/TrianglesScene'], function(TrianglesScene) {
 
     // fetch canvas element
     var canvas = window.document.getElementById("canvas");
 
     // instantiate and run scene
-    new demo.TrianglesScene(canvas).start();
+    new TrianglesScene(canvas).start();
 });
